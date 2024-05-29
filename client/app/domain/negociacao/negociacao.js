1b4e2d06a68d6c698b8c06a7a777d3eb616cf6fe
jonatas-campista <jonatasc5622@gmail.com>
2024-05-29 16:43:56 -0300

    ajustes class negociaçao

--- client/app/domain/negociacao/negociacao.js
+++ client/app/domain/negociacao/negociacao.js
@@ -1,25 +1,21 @@
 class Negociacao {
-
-    constructor(_data,	_quantidade,	_valor)	{
+    //	MUDANÇA	NO	NOME	DOS	PARÂMETROS,	AGORA	COM	UNDERLINE
+	constructor(_data,	_quantidade,	_valor)	{
+        //	USANDO	O	ATALHO	PARA	DECLARAÇÃO	DE	PROPRIEDADES
         Object.assign(this,	{	_quantidade,	_valor	});
         this._data	=	new Date(_data.getTime());
         Object.freeze(this);
-        }
-        
-
-    get Volume() {
+    }
+    get volume() {
         return this._quantidade * this._valor;
     }
-
-    get Data() {
+    get data() {
         return new Date(this._data.getTime());
     }
-
-    get Quantidade() {
+    get quantidade() {
         return this._quantidade;
     }
-
-    get Valor() {
+    get valor() {
         return this._valor;
     }
 }
